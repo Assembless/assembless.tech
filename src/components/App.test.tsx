54d8c9ae275ee500e4762e6c5e14b13b5aad695e
Mike Eling <mike.eling97@gmail.com>
2021-01-18 21:33:35 +0100

    :fire: Remove unused tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,3 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
-
-test('renders home text', () => {
-  render(<App />);
-  const el = screen.getByText(/Home/i);
-  expect(el).toBeInTheDocument();
-});
