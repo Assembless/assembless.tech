28321ed6646c6909caf46f45bc3ca30c986adefb
norwegianbeginner <r.cichocki.dcm@gmail.com>
2021-02-12 03:10:41 +0100

    :art: Improve style role view

--- src/pages/Career/Role/index.tsx
+++ src/pages/Career/Role/index.tsx
@@ -53,45 +53,45 @@ const CareerRole = (props: CareerRoleProps) => {
 
             <IconButton color="primary" onClick={handleOpen} ><Icon color="primary">launch</Icon></IconButton>
         </Box>
-        <Dialog open={open} onClose={handleClose} PaperProps={{ style: { width: "40vw", padding: '25px' } }}>
-            <Box style={{ display: 'flex', justifyContent: 'space-between', paddingBottom: '35px' }}>
+        <Dialog open={open} onClose={handleClose} PaperProps={{ style: { width: "40vw", padding: '40px' } }} maxWidth='lg'>
+            <Box style={{ display: 'flex', justifyContent: 'space-between', paddingBottom: '45px' }}>
                 <Box >
-                    <Typography variant="h5" style={{ fontFamily: "'PT Mono', monospace", marginBottom: '4px', fontSize: '32px' }}>{props.role.role}</Typography>
-                    {props.role.project && <Typography variant="h6" style={{ opacity: 0.6, fontSize: '18px' }}>{props.role.project.name}</Typography>}
+                    <Typography variant="h5" style={{ fontFamily: "'PT Mono', monospace", marginBottom: '4px', fontSize: '28px' }}>{props.role.role}</Typography>
+                    {props.role.project && <Typography variant="h6" style={{ opacity: 0.6, fontSize: '16px' }}>{props.role.project.name}</Typography>}
                 </Box>
                 <DialogActions style={{ padding: '0', alignItems: 'flex-start' }}>
                     <Button variant="text" onClick={handleClose}><Icon>close</Icon></Button>
                 </DialogActions>
             </Box>
-            <Box>
-                <Typography style={{ fontSize: '22px', marginBottom: '5px' }}>Tech Stack</Typography>
+            {props.role.requirements && <Box>
+                <Typography style={{ marginBottom: '5px', textTransform: 'uppercase', letterSpacing: '2px', opacity: '0.3' }}>Tech Stack</Typography>
                 <Box style={{ height: '2px', background: '#fff', opacity: '0.1' }}></Box>
-                <Box style={{ display: 'flex', flexWrap: 'wrap', paddingTop: '20px' }}>
-                    {props.role.requirements && props.role.requirements.map(MapRequriements(props.role.requirements))}
+                <Box style={{ display: 'flex', flexWrap: 'wrap', paddingTop: '15px' }}>
+                    {props.role.requirements.map(MapRequriements(props.role.requirements))}
                 </Box>
-            </Box>
-            <Box style={{ height: '2px', background: '#fff', opacity: '0.1', marginBottom: '25px' }}></Box>
+            </Box>}
+            <Box style={{ height: '2px', background: '#fff', opacity: '0.1', marginBottom: '12px' }}></Box>
             <DialogContent style={{ padding: '0' }}>
-                <Typography style={{ opacity: '0.7', }}>{props.role.description}</Typography>
+                <Typography style={{ opacity: '0.6', }}>{props.role.description}</Typography>
             </DialogContent>
-            <DialogActions style={{ padding: '0', alignItems: 'flex-end', marginTop: '25px' }}>
+            <DialogActions style={{ padding: '0', justifyContent: 'center', marginTop: '25px' }}>
                 <Button style={{ backgroundColor: '#2196f3', color: '#fff' }} variant="contained" onClick={handleOpenForm}>apply</Button>
             </DialogActions>
         </Dialog>
         <Dialog open={openForm} onClose={handleCloseForm} PaperProps={{ style: { width: '70vw', padding: '50px' } }} maxWidth='xl' >
-            <RecrutingForm role={props.role.role} questions={props.role.questions ?? undefined} />
+            <RecrutingForm role={props.role.role} questions={props.role.questions ?? undefined} handleCloseForm={handleCloseForm} />
         </Dialog>
     </>
 }
 
 const MapRequriements = (requirements: IRoleRequirements[]) => (requirement: IRoleRequirements) => (
-    <Box style={{ margin: '0 35px 20px 0', }}>
+    <Box style={{ margin: '0 35px 15px 0', }}>
         {[...Array(4)].map((el, index) => <Icon style={{ color: `${index < requirement.level ? '#2196f3' : 'primary'}`, fontSize: '16px', marginRight: '3px' }} >fiber_manual_record</Icon>)
         }
         <Typography style={{ fontSize: '18px', letterSpacing: '1px' }}>{requirement.label}</Typography>
     </Box >
 )
-const RecrutingForm = ({ role, questions, }: { role: string, questions?: string[] }) => {
+const RecrutingForm = ({ role, questions, handleCloseForm }: { role: string, questions?: string[], handleCloseForm: () => void }) => {
 
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
@@ -137,9 +137,16 @@ const RecrutingForm = ({ role, questions, }: { role: string, questions?: string[
 
     return (
         <form onSubmit={handleSubmit}>
-            <Typography variant="h3" style={{ fontSize: '16px', textTransform: 'uppercase', opacity: '0.4', letterSpacing: '1px', marginBottom: '5px' }}> {role}</Typography>
-            <Typography variant="h2" style={{ fontFamily: "'PT Mono', monospace", marginBottom: '40px', fontSize: '32px' }}>application form</Typography>
-            <Typography style={{ marginBottom: '35px', opacity: '0.7', width: '70%' }}>Laborum commodo anim incididunt dolor eu quis deserunt. Dolor est voluptate mollit non ut commodo. Consectetur veniam incididunt pariatur enim non non mollit eu velit qui do. Elit labore eiusmod ea eu ex irure anim commodo irure ipsum. Culpa eu sit officia veniam Lorem labore pariatur aliqua enim fugiat culpa id. Dolore mollit veniam nulla velit consectetur consequat duis cupidatat voluptate consectetur. Excepteur duis est ea exercitation sunt officia cillum pariatur sit aute qui excepteur.</Typography>
+            <Box display='flex' justifyContent='space-between'>
+                <Box style={{ width: '80%', }}>
+                    <Typography variant="h3" style={{ fontSize: '16px', textTransform: 'uppercase', opacity: '0.4', letterSpacing: '1px', marginBottom: '5px' }}> {role}</Typography>
+                    <Typography variant="h2" style={{ fontFamily: "'PT Mono', monospace", marginBottom: '40px', fontSize: '32px' }}>application form</Typography>
+                    <Typography style={{ marginBottom: '35px', opacity: '0.7' }}>Laborum commodo anim incididunt dolor eu quis deserunt. Dolor est voluptate mollit non ut commodo. Consectetur veniam incididunt pariatur enim non non mollit eu velit qui do. Elit labore eiusmod ea eu ex irure anim commodo irure ipsum. Culpa eu sit officia veniam Lorem labore pariatur aliqua enim fugiat culpa id. Dolore mollit veniam nulla velit consectetur consequat duis cupidatat voluptate consectetur. Excepteur duis est ea exercitation sunt officia cillum pariatur sit aute qui excepteur.</Typography>
+                </Box>
+                <Box style={{ display: 'flex', alignItems: 'flex-start' }}>
+                    <Button variant="text" onClick={handleCloseForm}><Icon>close</Icon></Button>
+                </Box>
+            </Box>
             <Box display='flex' flexDirection='column' style={{ marginBottom: '25px' }} >
                 <TextField autoComplete='off' style={{ marginBottom: '10px', backgroundColor: '#000', borderRadius: '4px' }} variant="filled" label="Name" name='first_name' value={firstName} onChange={handleChange} />
                 <TextField autoComplete='off' style={{ marginBottom: '10px', backgroundColor: '#000', borderRadius: '4px' }} variant="filled" label="Surname" name='last_name' value={lastName} onChange={handleChange} />
@@ -152,7 +159,7 @@ const RecrutingForm = ({ role, questions, }: { role: string, questions?: string[
                     <QuestionDiv question={question} index={index} handleChange={handleChange} questionsValue={questionsValue} />
                 ))
             }
-            <DialogActions style={{ padding: '0', justifyContent: 'center', marginTop: '50px' }}>
+            <DialogActions style={{ padding: '0', justifyContent: 'center', marginTop: '40px' }}>
                 <Button style={{ backgroundColor: '#2196f3', color: '#fff' }} variant="contained" size="large" type='submit'>Send application</Button>
             </DialogActions>
         </form>
