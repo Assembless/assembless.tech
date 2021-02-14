b26d589143b3d2c45fa0ac5ce108038f804041a6
Mike Eling <mike.eling97@gmail.com>
2021-02-15 00:29:38 +0100

    :tshirt: Fix CI build warnings

--- src/components/common/Header/Earth.tsx
+++ src/components/common/Header/Earth.tsx
@@ -1,14 +1,14 @@
 import React, { Suspense, useEffect, useRef } from 'react'
 import { extend, MeshProps, useFrame, useLoader, useThree } from 'react-three-fiber'
-import * as THREE from 'three'
-import 'three/examples/jsm/controls/OrbitControls'
-
+import { Mesh, TextureLoader } from 'three'
 // @ts-ignore
-extend({ OrbitControls: THREE.OrbitControls })
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+extend({ OrbitControls })
 
 const Earth: React.FC<MeshProps> = () => {
     // This reference will give us direct access to the mesh
-    const mesh = useRef<THREE.Mesh>()
+    const mesh = useRef<Mesh>()
 
     // Rotate mesh every frame, this is outside of React without overhead
     useFrame(() => {
@@ -30,11 +30,11 @@ const Earth: React.FC<MeshProps> = () => {
 }
 
 function Asset() {
-    const mesh = useRef<THREE.Mesh>()
-    const map = useLoader(THREE.TextureLoader, "/assets/8081_earthmap4k.jpg");
-    const spec = useLoader(THREE.TextureLoader, "/assets/8081_earthspec4k.jpg");
-    const lights = useLoader(THREE.TextureLoader, "/assets/8081_earthlights4k.jpg");
-    const bumpMap = useLoader(THREE.TextureLoader, "/assets/8081_earthbump4k.jpg");
+    const mesh = useRef<Mesh>()
+    const map = useLoader(TextureLoader, "/assets/8081_earthmap4k.jpg");
+    const spec = useLoader(TextureLoader, "/assets/8081_earthspec4k.jpg");
+    const lights = useLoader(TextureLoader, "/assets/8081_earthlights4k.jpg");
+    const bumpMap = useLoader(TextureLoader, "/assets/8081_earthbump4k.jpg");
 
     useFrame(() => {
         if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.0005
