87a1056e310ba328bf588c5380808c0f02485c40
Mike <mike.eling@assembless.tech>
2021-07-29 18:39:12 +0200

    🔧 Update linter

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Footer from 'common/Footer/';
+import Footer from './common/Footer/';
 
 test(`placeholder test`, () => {
   render(<Footer />);
